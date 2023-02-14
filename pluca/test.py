7993fd4ebe04baa099f32bc1e7c71895d625f75f
Flávio Veloso Soares <flaviovs@magnux.com>
2023-02-13 17:58:25 -0800

    Improve readability off the generic cache test class

--- pluca/test.py
+++ pluca/test.py
@@ -22,113 +22,113 @@ class CacheTester(abc.ABC, _BaseClass):
         self.get_cache()
 
     def test_put_get(self) -> None:
-        c = self.get_cache()
+        cache = self.get_cache()
         key1 = uuid.uuid4()
         value1 = uuid.uuid4()
         key2 = uuid.uuid4()
         value2 = uuid.uuid4()
-        c.put(key1, value1)
-        c.put(key2, value2)
-        self.assertEqual(c.get(key1), value1)
-        self.assertEqual(c.get(key2), value2)
+        cache.put(key1, value1)
+        cache.put(key2, value2)
+        self.assertEqual(cache.get(key1), value1)
+        self.assertEqual(cache.get(key2), value2)
 
     def test_put_get_check_key_types(self) -> None:
-        c = self.get_cache()
+        cache = self.get_cache()
         value1 = uuid.uuid4()
         value2 = uuid.uuid4()
 
-        c.put(1, value1)
-        c.put('1', value2)
+        cache.put(1, value1)
+        cache.put('1', value2)
 
-        self.assertEqual(c.get(1), value1)
-        self.assertEqual(c.get('1'), value2)
+        self.assertEqual(cache.get(1), value1)
+        self.assertEqual(cache.get('1'), value2)
 
     def test_get_default(self) -> None:
-        c = self.get_cache()
-        self.assertEqual(c.get('nonexistent', 'default'), 'default')
+        cache = self.get_cache()
+        self.assertEqual(cache.get('nonexistent', 'default'), 'default')
 
     def test_put_max_age(self) -> None:
-        c = self.get_cache()
+        cache = self.get_cache()
         key = uuid.uuid4()
         value = uuid.uuid4()
-        c.put(key, value, 1)  # Expires in 1 second.
+        cache.put(key, value, 1)  # Expires in 1 second.
         time.sleep(1)
         with self.assertRaises(KeyError):
-            c.get(key)
+            cache.get(key)
 
     def test_put_tuple_key(self) -> None:
-        c = self.get_cache()
+        cache = self.get_cache()
         key = (uuid.uuid4(), uuid.uuid4())
         value = uuid.uuid4()
-        c.put(key, value)
-        self.assertEqual(c.get(key), value)
+        cache.put(key, value)
+        self.assertEqual(cache.get(key), value)
 
     def test_put_list_key(self) -> None:
-        c = self.get_cache()
+        cache = self.get_cache()
         key = [uuid.uuid4(), uuid.uuid4()]
         value = uuid.uuid4()
-        c.put(key, value)
-        self.assertEqual(c.get(key), value)
+        cache.put(key, value)
+        self.assertEqual(cache.get(key), value)
 
     def test_put_dict_key(self) -> None:
-        c = self.get_cache()
+        cache = self.get_cache()
         key = {uuid.uuid4(): uuid.uuid4()}
         value = uuid.uuid4()
-        c.put(key, value)
-        self.assertEqual(c.get(key), value)
+        cache.put(key, value)
+        self.assertEqual(cache.get(key), value)
 
     def test_remove(self) -> None:
-        c = self.get_cache()
+        cache = self.get_cache()
         key = uuid.uuid4()
-        c.put(key, 'value')
-        c.remove(key)
+        cache.put(key, 'value')
+        cache.remove(key)
         with self.assertRaises(KeyError):
-            c.get(key)
+            cache.get(key)
 
     def test_remove_nonexistent(self) -> None:
-        c = self.get_cache()
+        cache = self.get_cache()
         with self.assertRaises(KeyError):
-            c.remove('nonexistent')
+            cache.remove('nonexistent')
 
     def test_flush(self) -> None:
-        c = self.get_cache()
+        cache = self.get_cache()
         key1 = uuid.uuid4()
         key2 = uuid.uuid4()
-        c.put(key1, 'value1')
-        c.put(key2, 'value2')
-        c.flush()
+        cache.put(key1, 'value1')
+        cache.put(key2, 'value2')
+        cache.flush()
         with self.assertRaises(KeyError):
-            c.get(key1)
+            cache.get(key1)
         with self.assertRaises(KeyError):
-            c.get(key2)
+            cache.get(key2)
 
     def test_has(self) -> None:
-        c = self.get_cache()
+        cache = self.get_cache()
         key = uuid.uuid4()
         value = uuid.uuid4()
-        c.put(key, value)
-        self.assertTrue(c.has(key))
-        self.assertFalse(c.has('nonexistentkey'))
+        cache.put(key, value)
+        self.assertTrue(cache.has(key))
+        self.assertFalse(cache.has('nonexistentkey'))
 
     def test_put_many(self) -> None:
-        c = self.get_cache()
+        cache = self.get_cache()
         key1 = uuid.uuid4()
         value1 = uuid.uuid4()
         key2 = uuid.uuid4()
         value2 = uuid.uuid4()
-        c.put_many({key1: value1, key2: value2})
-        self.assertEqual(c.get(key1), value1)
-        self.assertEqual(c.get(key2), value2)
+        cache.put_many({key1: value1, key2: value2})
+        self.assertEqual(cache.get(key1), value1)
+        self.assertEqual(cache.get(key2), value2)
 
     def test_get_many(self) -> None:
-        c = self.get_cache()
+        cache = self.get_cache()
         key1 = uuid.uuid4()
         value1 = uuid.uuid4()
         key2 = uuid.uuid4()
         value2 = uuid.uuid4()
-        c.put(key1, value1)
-        c.put(key2, value2)
-        res = dict(c.get_many([key1, key2, 'nonexistent']))
+        cache.put(key1, value1)
+        cache.put(key2, value2)
+        res = dict(cache.get_many([key1, key2, 'nonexistent']))
         self.assertIn(key1, res)
         self.assertEqual(res[key1], value1)
         self.assertIn(key2, res)
@@ -136,11 +136,11 @@ class CacheTester(abc.ABC, _BaseClass):
         self.assertNotIn('nonexistent', res)
 
     def test_get_many_default(self) -> None:
-        c = self.get_cache()
+        cache = self.get_cache()
         key = uuid.uuid4()
         value = uuid.uuid4()
-        c.put(key, value)
-        res = dict(c.get_many([key, 'nonexistent'], 'default'))
+        cache.put(key, value)
+        res = dict(cache.get_many([key, 'nonexistent'], 'default'))
 
         self.assertIn(key, res)
         self.assertEqual(res[key], value)

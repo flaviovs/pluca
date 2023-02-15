df13140176baf07fc496ddf6f05fca370fbbc1b5
Flávio Veloso Soares <flaviovs@magnux.com>
2023-02-15 03:39:03 -0800

    Add Docker Compose support for testing DBs and multiple Python versions

--- qa.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-ROOT=$(git rev-parse --show-toplevel)
-
-docker run --rm -v "$ROOT:/app" -w /app python:3.7-slim \
-       python -m doctest README.md
-
-docker run --rm -v "$ROOT:/app" -w /app python:3.7-slim \
-       python -m unittest
